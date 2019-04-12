6e257a822d4e30c751852ce0a3053ef21f93c544
Edward <t102590050@ntut.org.tw>
2019-04-12 22:25:47 +0800

    Fifth Version
    - Change info page to photos carousel

--- js/lang.js
+++ js/lang.js
@@ -1,10 +1,9 @@
 function setLanguage(lang) {
-	console.log();
 	$.getJSON('lang/' + lang + '.json', translate);
 	$('#en').removeClass('hide-page');
-	  $('#zh').removeClass('hide-page');
-	  $('#es').removeClass('hide-page');
-  	$('#' + lang).addClass('hide-page');
+	$('#zh').removeClass('hide-page');
+	$('#es').removeClass('hide-page');
+	$('#' + lang).addClass('hide-page');
 }
 
 var translate = function (jsdata) {	
@@ -13,10 +12,11 @@ var translate = function (jsdata) {
 	});
 };
 
+var lang_list=['en','zh','es'];
 var language = window.navigator.language.substr(0,2);
-if (language=='zh') {
-	setLanguage(language);
-}
-else {
-	setLanguage('en');
+setLanguage(lang_list[0]);
+for (var i=1; i < lang_list.length; i++){
+	if (language==lang_list[i]) {
+		setLanguage(lang_list[i]);
+	}
 }
